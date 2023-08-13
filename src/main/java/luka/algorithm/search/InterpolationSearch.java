0e139fe41e69e2f57834f82b44d9d4b3bf4934b3
ba-luan <73507718+ba-luan@users.noreply.github.com>
2023-08-13 11:53:40 -0400

    Add algorithm Jump Search and Interpolation Search.

---
+++ src/main/java/luka/algorithm/search/InterpolationSearch.java
@@ -0,0 +1,53 @@
+package luka.algorithm.search;
+
+public class InterpolationSearch {
+    public static int interpolationSearch(int[] array, int target) {
+        int left = 0;
+        int right = array.length - 1;
+
+        while (left <= right && target >= array[left] && target <= array[right]) {
+            /*
+            Estimate the position using interpolation formula:
+
+            int deltaValue = (target - array[left]);
+            int valueRange = (array[right] - array[left]);
+            int weight = deltaValue / valueRange;
+            int indexRange = (right - left);
+            int pos = left + weight * indexRange;
+            */
+            int pos = left + ((target - array[left]) * (right - left)) / (array[right] - array[left]);
+
+            if (array[pos] == target) {
+                return pos; // Target found
+            }
+
+            if (array[pos] < target) {
+                left = pos + 1; // Search in the right half
+            } else {
+                right = pos - 1; // Search in the left half
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+
+        /*
+         Example of worst scenario: bias-distributed data and target is the second last element in array.
+         Iterate to every element. Time complexity O(n) instead of average scenario O(log log n).
+        */
+
+        int[] array = { 2, 4, 6, 8, 10, 12, 14, 16, 18 , 20, 999999};
+        int target = 20;
+
+        int index = interpolationSearch(array, target);
+
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+}
+
---
+++ src/main/java/luka/algorithm/search/JumpSearch.java
@@ -0,0 +1,44 @@
+package luka.algorithm.search;
+
+public class JumpSearch {
+    public static int jumpSearch(int[] array, int target) {
+        int n = array.length;
+        int blockSize = (int) Math.sqrt(n); // Calculate block size
+
+        int step = blockSize;
+        int prev = 0;
+
+        // Jump forward in blocks
+        while (array[Math.min(step, n) - 1] < target) {
+            prev = step;
+            step += blockSize;
+
+            if (prev >= n) {
+                return -1; // Target not found
+            }
+        }
+
+        // Linear search within the identified block
+        for (int i = prev; i < Math.min(step, n); i++) {
+            if (array[i] == target) {
+                return i; // Target found
+            }
+        }
+
+        return -1; // Target not found
+    }
+
+    public static void main(String[] args) {
+        int[] array = { 2, 4, 6, 8, 10, 12, 14, 16, 18 };
+        int target = 10;
+
+        int index = jumpSearch(array, target);
+
+        if (index != -1) {
+            System.out.println("Element " + target + " found at index " + index);
+        } else {
+            System.out.println("Element " + target + " not found in the array");
+        }
+    }
+}
+
